92e4ca4f520c7c28141f75dec426916539a463a3
techtorialdevops <130173463+techtorialdevops@users.noreply.github.com>
2024-05-08 20:26:47 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -80,6 +80,8 @@ resource "aws_db_instance" "db_instance" {
   password              = "password"
   multi_az              = true
   publicly_accessible   = false
+  skip_final_snapshot   = true
+  final_snapshot_identifier = "terraform-20240509003343928000000001-snapshot"
 
   tags = {
     Name = "db-instance-${count.index}"
@@ -162,4 +164,60 @@ resource "aws_lb_listener_rule" "app_listener_rule" {
       values = ["example.com"]  # Replace with your domain name
     }
   }
+}
+
+# Create a public route table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.terraform6.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.example.id  # Use the internet gateway created earlier
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+# Associate public subnets with the public route table
+resource "aws_route_table_association" "public_subnet_association" {
+  count          = 3
+  subnet_id      = aws_subnet.public_subnets[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+# Create a private route table
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.terraform6.id
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+# Create NAT gateway(s)
+resource "aws_nat_gateway" "nat_gateway" {
+  count        = 3  # One NAT gateway per availability zone
+  subnet_id    = aws_subnet.public_subnets[count.index].id
+  allocation_id = aws_eip.nat_eip[count.index].id
+
+  tags = {
+    Name = "nat-gateway-${count.index}"
+  }
+}
+
+# Create Elastic IPs for the NAT gateways
+resource "aws_eip" "nat_eip" {
+  count = 3
+
+  vpc      = true
+}
+
+# Create a route in the private route table to route traffic through the NAT gateway
+resource "aws_route" "private_route" {
+  count          = 3
+  route_table_id = aws_route_table.private_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.nat_gateway[count.index].id
 }
